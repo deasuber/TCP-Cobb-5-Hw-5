3d4e8dda94455464a8e355edb8967f35b2edd1fa
deasuber <72317810+deasuber@users.noreply.github.com>
2020-11-27 13:37:39 -0500

    rework

--- Develop/script.js
+++ Develop/script.js
@@ -1,70 +1,61 @@
 // Assignment Code
-
-const characterAmountRange= document.getElementById('characterAmountRange')
-const characterAmountNumber= document.getElementById('characterAmountNumber')
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
 const includeNumbersElement = document.getElementById('includeNumbers')
 const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById("passwordGeneratorForm")
+const passwordDisplay = document.getElementById('passwordDisplay')
 
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-characterAmountNumber.addEventListener('input',syncCharacterAmount)
-characterAmountRange.addEventListener('input',syncCharacterAmount)
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-form.addEventListener('submit', e=>{
+form.addEventListener('submit', e => {
   e.preventDefault()
   const characterAmount = characterAmountNumber.value
   const includeUppercase = includeUppercaseElement.checked
-  const Numbers = includeNumbersElement.checked
+  const includeNumbers = includeNumbersElement.checked
   const includeSymbols = includeSymbolsElement.checked
   const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
 })
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols){
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
-  if(includeUppercase) charCodes.concat(UPPERCASE_CHAR_CODES)
-  if(includeNumbers) charCodes.concat(NUMBER_CHAR_CODES)
-  if(includeSymbols) charCodes.concat(SYMBOL_CHAR_CODES)
-
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
   const passwordCharacters = []
-  for (let i = 0; i <characterAmount, i++){
-    const characterCode = charCodes[Math.floor(Math.random()*charCodes.length)]
-    passwordCharters.push(String.fromCharCode(characterCode))
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
   }
-  return passwordCharters.join('')
+  return passwordCharacters.join('')
 }
 
-function arrayFromLowToHigh(low, high){
+function arrayFromLowToHigh(low, high) {
   const array = []
-  for(let i = low; i<=high; i++){
+  for (let i = low; i <= high; i++) {
     array.push(i)
   }
   return array
 }
 
-function syncCharacterAmount(e){
+function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
   characterAmountRange.value = value
 }
 
-
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
